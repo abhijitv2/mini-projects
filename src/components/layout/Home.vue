17367354fbae35c48ded31898072843e484030ab
abhijitv2 <abhijit.sawant@v2solutions.com>
2023-11-01 17:52:32 +0530

    added changes to the landing page

--- src/components/layout/Home.vue
+++ src/components/layout/Home.vue
@@ -1,13 +1,77 @@
 <template>
-<div class="mini-projects">
-    <RouterLink to="/note-taking-app" class="app-card">Note Taking App</RouterLink>
-    <RouterLink to="/task-manager" class="app-card">Task Manager</RouterLink>
-    <RouterLink to="/weather-app" class="app-card">Weather App</RouterLink>
-    <RouterLink to="/trivia-quiz" class="app-card">Trivia Quiz</RouterLink>
-    <RouterLink to="/github-profile-viewer" class="app-card">Github Profile</RouterLink>
-    <RouterLink to="/movie-search-app" class="app-card">Movie Search App</RouterLink>
-    <RouterLink to="/ecommerce-product-list" class="app-card">Ecommerce Product List</RouterLink>
-</div>
+    <div class="mini-projects">
+
+        <div class="app-card" @click="$router.push('/note-taking-app')">
+            <div class="app-card-image1">
+                <img src="../../assets/images/note.jpg" />
+            </div>
+            <div class="app-card-detail1">
+                <h5 class="app-name1">Note Taking App</h5>
+                <p class="app-desc1">This app allows you to create new notes, edit and delete them.</p>
+            </div>
+        </div>
+
+        <div class="app-card" @click="$router.push('/task-manager')">
+            <div class="app-card-image2">
+            </div>
+            <div class="app-card-detail2">
+                <h5 class="app-name2">Task Manager App</h5>
+                <p class="app-desc2">Allows you to create new task, edit and mark them as completed</p>
+            </div>
+        </div>
+
+        <div class="app-card" @click="$router.push('/weather-app')">
+            <div class="app-card-image3">
+            </div>
+            <div class="app-card-detail3">
+                <h5 class="app-name2">Weather App</h5>
+                <p class="app-desc2">Know the weather condition of any city.</p>
+            </div>
+        </div>
+
+        <div class="app-card" @click="$router.push('/trivia-quiz')">
+            <div class="app-card-image4">
+            </div>
+            <div class="app-card-detail4">
+                <h5 class="app-name2">Trivia Quiz</h5>
+                <p class="app-desc2">Play Quiz and Test your Knowledge</p>
+            </div>
+        </div>
+
+
+        <div class="app-card" @click="$router.push('/github-profile-viewer')">
+            <div class="app-card-image5">
+            </div>
+            <div class="app-card-detail5">
+                <h5 class="app-name2">Github Profile Viewer</h5>
+                <p class="app-desc2">Search and View any Github profile</p>
+            </div>
+        </div>
+
+        <div class="app-card" @click="$router.push('/movie-search-app')">
+            <div class="app-card-image6">
+            </div>
+            <div class="app-card-detail6">
+                <h5 class="app-name2">Movie Search App</h5>
+                <p class="app-desc2">Search and View your favorite Movie details</p>
+            </div>
+        </div>
+
+
+        <div class="app-card" @click="$router.push('/ecommerce-product-list')">
+            <div class="app-card-image7">
+            </div>
+            <div class="app-card-detail7">
+                <h5 class="app-name2">Ecommerce Product List</h5>
+                <p class="app-desc2">Search and View your favorite Movie details</p>
+            </div>
+        </div>
+
+
+
+
+
+    </div>
 </template>
 
 <script setup>
